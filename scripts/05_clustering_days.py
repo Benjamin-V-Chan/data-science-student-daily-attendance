6dd3f95ce9da718f8cad25e6b269274da857315f
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-26 23:24:33 -0700

    05_clustering_days script outline

---
+++ scripts/05_clustering_days.py
@@ -0,0 +1,6 @@
+# Import libraries
+# Load cleaned data
+# Create features for clustering: AbsenceRate, AttendanceRate, Released
+# Use KMeans to cluster the data into 'types' of days (e.g., 3 clusters)
+# Visualize cluster centers
+# Save cluster assignments and visualizations
